cca6681f076577033582817f7b8e664a6a3f7fff
6lexicon6devil6 <alrschul@ucsc.edu>
2021-01-27 21:34:09 -0800

    yuh

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/**
+ * Author: Alexis Schulz
+ * Created: 01.27.2021
+ * License: Public Domain
+ **/
+
+ // Define Variables
+ make = "Dodge";
+ model = "Dakota";
+ color = "White";
+ year = 1997;
+
+ // calculate
+ age = 2021 - year;
+
+ // output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " years<br>");
