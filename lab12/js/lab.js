0cbb31c0686b824fe759a0f2dcb6dd0f6de8198c
6lexicon6devil6 <alrschul@ucsc.edu>
2021-03-08 22:45:48 -0800

    yes

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -6,26 +6,30 @@
 
  // Return the houses
 function.sortingHat(str) {
-  len = str.length;
-  mod = len % 4;
-  if (mod == 0) {
+  length = str.length;
+  mod = length % 4;
+  if (mod == 0){
     return "Gryffindor"
   }
-  else if (mod == 1) {
+  else if (mod == 1){
     return "Ravenclaw"
   }
-  else if (mod == 2) {
+  else if (mod == 2){
     return "Slytherin"
   }
-  else if (mod == 3) {
+  else if (mod == 3){
     return "Hufflepuff"
   }
 }
 
+//create an event listener attached to #button
 var myButton = document.getElementById("button");
-myButton.addEventListener("click", function() {
+myButton.addEventListener("click", function(){
+//that gets the value of #input and assigns it to a variable name
   var name = document.getElementById("input").value;
+//appends a new styled paragraph to #output that says "The Sorting Hat has sorted you into" + house
   var house = sortingHat(name);
-  newText = "<p>"The Sorting Hat has sorted you into " + house + "</p>";
-  document.getElementById("ouput").innerHTML = newText;
-}
+//appends a new styled paragraph to output that says "The Sorting Hat has sorted you"
+  newText = "<p>The Sorting Hat has sorted you into " + house + "</p>";
+  document.getElementById("output").innerHTML = newText;
+})
